23ea0f424a621bc974a64d35d6d4ecd1837f183c
Javier Nebot <javier.nebotalminana@atos.net>
2023-05-30 10:30:04 +0200

    Fin Part-2.

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from '@/components/Header';
-import TodosLogic from "./TodosLogic";
+import TodosLogic from "@/components/TodosLogic";
 
 const TodoApp = () => {
 	return (
